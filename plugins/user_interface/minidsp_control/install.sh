677a4c82e492d8a8399b08cdbeae48873847bf0a
Mark Kubiak <mkubiak.dev@gmail.com>
2017-09-25 20:38:19 +0000

    resolved install issues

--- plugins/user_interface/minidsp_control/install.sh
+++ plugins/user_interface/minidsp_control/install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 echo "Installing minidsp control Dependencies"
+
+# node-hid deps for building from source
+sudo apt install -y build-essential git libusb-1.0-0 libusb-1.0-0-dev
+sudo npm install -g node-gyp
+
 cd /data/plugins/user_interface/minidsp_control
 npm install
+
+# copy udev rule
+sudo cp 99-minidsp.rules /etc/udev/rules.d
+sudo udevadm control --reload-rules
 
 #requred to end the plugin install
 echo "plugininstallend"
--- plugins/user_interface/minidsp_control/uninstall.sh
+++ plugins/user_interface/minidsp_control/uninstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Uninstall dependendencies
+# leave build-essential and git installed because they're very useful
+sudo apt remove -y libusb-1.0-0 libusb-1.0-0-dev
+sudo npm uninstall -g node-gyp
 
 echo "Done"
 echo "pluginuninstallend"
